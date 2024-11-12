2342e74dd6ff1c034f7f09d1d6e93a314f5a4a98
Antony Lajes <tonymateuslajes@hotmail.com>
2024-11-12 19:24:27 -0300

    chore: added sqlite conn & .env variables

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
 import fastify from 'fastify'
+import { env } from '../env'
 import { knex } from './database'
 
 const server = fastify()
 
 server.get('/hello', async () => {
     const transactions = await knex('transactions')
-        .where('title', 'Nova transação')
+        .where('title', 'Nova transação 3')
         .select("*")
 
     return transactions
 })
 
 server.listen({
-    port: 3333
+    port: env.API_PORT
 }).then(() => {
     console.log('HTTP server running!')
 })
